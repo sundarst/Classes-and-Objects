5caf490451307de992b236c35226df9642d1c5c9
Shunmugasundaram <129269793+sundarst@users.noreply.github.com>
2025-05-15 21:53:16 +0530

    MovieDto.java

--- MovieDto.java
+++ MovieDto.java
@@ -1,5 +1,26 @@
 package classAndObject;
 
-public class MovieDto {
+public class MovieDTO {
 
+    private String movieName;
+    private String director;
+    private int releaseYear;
+    private static String industry = "Bollywood";
+
+    public void showMovieInfo() {
+        System.out.println(movieName + " | " + director + " | " + releaseYear);
+    }
+
+    public static void showIndustry() {
+        System.out.println("Industry: " + industry);
+    }
+
+    public static void main(String[] args) {
+        showIndustry();
+        MovieDTO m1 = new MovieDTO();
+        m1.movieName = "3 Idiots";
+        m1.director = "Rajkumar Hirani";
+        m1.releaseYear = 2009;
+        m1.showMovieInfo();
+    }
 }

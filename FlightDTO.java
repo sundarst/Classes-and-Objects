ca9d24ced30842918cacb08fc2556883c1beed52
Shunmugasundaram <129269793+sundarst@users.noreply.github.com>
2025-05-15 21:49:03 +0530

    FlightDTO.java

--- FlightDTO.java
+++ FlightDTO.java
@@ -2,19 +2,23 @@ package classAndObject;
 
 public class FlightDTO {
 
-	String person;
-	String from;
-	String to;
-	static  String ticketAmount;
-	
-	public void indianAirline()
-	{
-		System.out.println("Welcome to Indian Airline");
-	}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		
-	}
+    private String person;
+    private String from;
+    private String to;
+    private static String ticketAmount = "5000";
 
+    public void showFlightInfo() {
+        System.out.println("Passenger: " + person + ", From: " + from + ", To: " + to);
+    }
+
+    public static void showAirlineName() {
+        System.out.println("Welcome to Indian Airline");
+    }
+
+	public static void main(String args [])
+	{
+		 FlightDTO  flightDTO= new  FlightDTO();
+		flightDTO.showFlightInfo();
+	         showAirlineName()
+	}
 }
